2eb5ab7c4cb019a9271ccb4a24f9c6e6706dfda6
Ev1lbl0w <Ev1lbl0w@users.noreply.github.com>
2022-02-20 19:59:57 +0000

    deploy: d259a94cdf8ebad1ac7e71f8cd1a88dc96ca5a0b

--- article/re-skgba/encode.sh
+++
@@ -1,7 +0,0 @@
-encode() {
-    echo "[+] Encoding $1.mp4"
-    ffmpeg -i $1.avi -c:v libx265 -crf 20 -c:a aac -b:a 128k $1.mp4
-    echo "[+] Encoding $1.webm"
-    ffmpeg -i $1.avi -c:v libvpx-vp9 -b:v 0 -crf 40 -pass 1 -an -f webm -deadline best /dev/null && \
-    ffmpeg -i $1.avi -c:v libvpx-vp9 -b:v 0 -crf 40 -pass 2 -deadline best -c:a libopus $1.webm
-}
